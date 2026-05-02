954087b2a85e2fd60dba2fcac73a6b656df2f76c
bennyclouds <benjamin.k.frey@gmail.com>
2026-05-01 20:23:31 -0500

    Create Index.ts to aggregate address datasets

---
+++ src/sandbox/datasets/Index.ts
@@ -0,0 +1,11 @@
+import { AIRPORT_ADDRESSES } from "./airports";
+import { HOTEL_ADDRESSES } from "./hotels";
+import { RESIDENTIAL_ADDRESSES } from "./residential";
+import { VENUE_ADDRESSES } from "./venues";
+
+export const ADDRESS_DATASET = {
+  airports: AIRPORT_ADDRESSES,
+  hotels: HOTEL_ADDRESSES,
+  residential: RESIDENTIAL_ADDRESSES,
+  venues: VENUE_ADDRESSES,
+};
