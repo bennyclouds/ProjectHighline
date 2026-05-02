594a7d37802126125a1e21893b566cd1b1c49bd1
bennyclouds <benjamin.k.frey@gmail.com>
2026-05-01 20:22:58 -0500

    Create VENUES_ADDRESSES with venue information
    Added a list of venue addresses in VENUE_ADDRESSES constant.

---
+++ src/sandbox/datasets/Venues.ts
@@ -0,0 +1,44 @@
+import { AddressRecord } from "./AddressRecord";
+
+export const VENUE_ADDRESSES: AddressRecord[] = [
+  {
+    label: "McCormick Place",
+    street1: "2301 S King Dr",
+    city: "Chicago",
+    state: "IL",
+    postalCode: "60616",
+    country: "US",
+  },
+  {
+    label: "United Center",
+    street1: "1901 W Madison St",
+    city: "Chicago",
+    state: "IL",
+    postalCode: "60612",
+    country: "US",
+  },
+  {
+    label: "Navy Pier",
+    street1: "600 E Grand Ave",
+    city: "Chicago",
+    state: "IL",
+    postalCode: "60611",
+    country: "US",
+  },
+  {
+    label: "Allstate Arena",
+    street1: "6920 Mannheim Rd",
+    city: "Rosemont",
+    state: "IL",
+    postalCode: "60018",
+    country: "US",
+  },
+  {
+    label: "Donald E. Stephens Convention Center",
+    street1: "5555 N River Rd",
+    city: "Rosemont",
+    state: "IL",
+    postalCode: "60018",
+    country: "US",
+  },
+];
